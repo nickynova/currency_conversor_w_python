d3c8bb7c45df365998beeafe1f438d2ebfb23558
Nick Rodriguez <nickynova@aol.com>
2020-09-15 09:48:36 -0500

    three currencies added

--- conversor.py
+++ conversor.py
@@ -1,19 +1,40 @@
-def foreign_exchange_calculator(amount):
-    mexc_to_cop_rate = 145.97
-
-    return mexc_to_cop_rate * amount
-
+def foreign_exchange_calculator(amount, main_currency, foreign_currency):
+    mxn_to_cop_rate = 179.79
+    mxn_to_usd_rate = 0.048
+    cop_to_mxn_rate = 0.0057
+    cop_to_usd_rate = 0.00027
+    usd_to_cop_rate = 3676
+    usd_to_mxn_rate = 21.01
+    
+    if main_currency == 'mxn' and foreign_currency == 'cop':
+        return mxn_to_cop_rate * amount
+    elif main_currency == 'mxn' and foreign_currency == 'usd':
+        return mxn_to_usd_rate * amount
+    elif main_currency == 'cop' and foreign_currency == 'mxn':
+        return cop_to_mxn_rate * amount 
+    elif main_currency == 'cop' and foreign_currency == 'usd':
+        return cop_to_usd_rate * amount 
+    elif main_currency == 'usd' and foreign_currency == 'mxn':
+        return usd_to_mxn_rate * amount 
+    elif main_currency == 'usd' and foreign_currency == 'cop':
+        return usd_to_cop_rate * amount 
+   
 
 def run():
     print('welcome to the foreign exchange calculator! ')
-    print('you can convert MXC to COP')
+    print('please insert the amount you want to convert and then the and the currency: ')
+    print('insert the Currency as its short form ex: cop, usd, mxn')
     print('')
 
-    amount = float(input('insert the amount of MXC you want to convert: '))
+    amount = float(input('insert the amount: '))
 
-    result = foreign_exchange_calculator(amount)
+    main_currency = input('insert your currency: ')
 
-    print('${} mexican pesos is ${} colombian pesos'.format(amount, int(result)))
+    foreign_currency = input('insert the currency you want to convert to: ')
+
+    result = foreign_exchange_calculator(amount, main_currency, foreign_currency)
+
+    print('${} ' + foreign_currency + ' is ' + '${} ' + main_currency.format(amount, result))
 
 
 if __name__ == "__main__":
